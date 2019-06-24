497d1e157f927c7165c45086f19b980496ee516c
huylv69 <levanhuy96@gmail.com>
2019-06-24 21:15:38 +0700

    init

--- index.js
+++ index.js
@@ -2,17 +2,29 @@ var app = require('express')();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
 var port = process.env.PORT || 3000;
+// Imports the Google Cloud client library
+var { translate } = require("google-translate-api-browser");
+// Instantiates a client
 
-app.get('/', function(req, res){
-  res.sendFile(__dirname + '/index.html');
+app.get('/', function (req, res) {
+    res.sendFile(__dirname + '/index.html');
 });
 
-io.on('connection', function(socket){
-  socket.on('chat message', function(msg){
-    io.emit('chat message', msg);
-  });
+io.on('connection', function (socket) {
+    socket.on('new-message', function (msg) {
+        translate(msg.data, { from: 'en', to: 'vi' }).then(res => {
+            io.emit('new-message',msg);
+            io.emit('new-message', {
+                data: res.text,
+                from: "bot",
+                id: socket.id
+            });
+        }).catch(err => {
+            console.error(err);
+        });
+    });
 });
 
-http.listen(port, function(){
-  console.log('listening on *:' + port);
+http.listen(port, function () {
+    console.log('listening on *:' + port);
 });
